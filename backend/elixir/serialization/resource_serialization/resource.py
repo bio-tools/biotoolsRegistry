0aaa07df43ed0b30a29b2f418cdd32809edd66af
Hans Ienasescu <hans@bio.tools>
2020-07-14 10:34:35 +0300

    Fixed tool annotation property lists ordering. Order is now kept.

--- backend/elixir/serialization/resource_serialization/resource.py
+++ backend/elixir/serialization/resource_serialization/resource.py
@@ -21,7 +21,7 @@ from elixir.serialization.resource_serialization.community import *
 from elixir.issues import EDAMTopicIssue, EDAMOperationIssue, EDAMDataIssue, EDAMFormatIssue, NoLicenseIssue, NoContactIssue, NoTOSIssue
 from random import randint
 from rest_framework.validators import UniqueValidator
-
+from orderedset import OrderedSet
 
 def issue_function(resource, user):
 	pass
@@ -390,7 +390,7 @@ class ResourceSerializer(serializers.ModelSerializer):
 	# creating the resource
 	def create(self, validated_data):
 		pop = lambda l, k: l.pop(k) if k in l.keys() else []
-		uniq = lambda l, k: [dict(t) for t in set([tuple(d.items()) for d in pop(l, k)])]
+		uniq = lambda l, k: [dict(t) for t in OrderedSet([tuple(d.items()) for d in pop(l, k)])]
 
 		# nested attributes need to be popped from resource and added after resource has been saved
 		# otherwise nothing will work
