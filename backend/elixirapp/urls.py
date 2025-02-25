72009e3cb20ce867ac868d5e41c17e4d6ea00cc6
anitamnd <anamendesml@outlook.com>
2025-02-25 14:55:28 +0100

    import path

--- backend/elixirapp/urls.py
+++ backend/elixirapp/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
 	1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
