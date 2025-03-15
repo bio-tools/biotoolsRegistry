4425295470bff45c258f7c6a6140746c4fbb7254
anitamnd <anamendesml@outlook.com>
2025-03-15 10:17:12 +0100

    fixes key error

--- backend/elixir/adapters.py
+++ backend/elixir/adapters.py
@@ -4,7 +4,9 @@ from django.conf import settings
 class CustomDefaultAccountAdapter(DefaultAccountAdapter):
 
 	def send_mail(self, template_prefix, email, context):
+		key = context.get('key') or context.get('token')
 		context['activate_url'] = settings.URL_FRONT + \
-			'signup/verify-email/' + context['key']
+			'signup/verify-email/' + key
 		msg = self.render_mail(template_prefix, email, context)
 		msg.send()
+
