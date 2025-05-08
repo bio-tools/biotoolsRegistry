2769ebb34eba75fabacb15e58e64c67a9222d0e3
anitamnd <anamendesml@outlook.com>
2025-05-08 14:24:57 +0200

    store cookie preferences

--- frontend/components/cookieBanner/cookieBanner.js
+++ frontend/components/cookieBanner/cookieBanner.js
@@ -8,22 +8,28 @@ angular.module('elixir_front')
 })
 .controller('CookieController', ['$scope', function($scope) {
   $scope.cookiesAccepted = localStorage.getItem('cookiesAccepted') === 'true';
+  $scope.cookiesPreferenceSet = localStorage.getItem('cookiesPreferenceSet') === 'true';
 
   $scope.acceptRequiredCookies = function() {
     $scope.cookiesAccepted = true;
+    $scope.cookiesPreferenceSet = true;
     localStorage.setItem('cookiesAccepted', 'required');
+    localStorage.setItem('cookiesPreferenceSet', 'true');
   };
 
   $scope.acceptCookies = function() {
     $scope.cookiesAccepted = true;
+    $scope.cookiesPreferenceSet = true;
     localStorage.setItem('cookiesAccepted', 'true');
+    localStorage.setItem('cookiesPreferenceSet', 'true');
+
   
     // Load Google Analytics
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  
+    
     ga('create', 'G-9RWP2H84XB');
     ga('send', 'pageview');
   };
