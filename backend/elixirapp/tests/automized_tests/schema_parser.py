c8c26a8e366fcfe22b8c06bb7f9a497cefb36259
alexandradenk <xendi3008@gmail.com>
2025-05-09 12:08:22 +0200

    adapted SchemaParser to parse own types

--- backend/elixirapp/tests/automized_tests/schema_parser.py
+++ backend/elixirapp/tests/automized_tests/schema_parser.py
@@ -10,12 +10,6 @@ TYPE_ARRAY_STR = "array"
 
 ARRAY_PATH_EXTENSION = ["items", "properties"]
 
-# automatically created : ("string": ["name": {"minLength": 1, ..., "path": ["parent1", "parent2", ...]]}])
-test_dict = {
-    TYPE_STRING_STR: [],
-    TYPE_OBJECT_STR: [],
-    TYPE_ARRAY_STR: []
-}
 
 # specifies constraints for data types; can/should be extended (e.g. numbers, different restrictions)
 type_dict = {  # data types and constraints (e.g. "string": ["minLength", "pattern"]
@@ -25,12 +19,19 @@ type_dict = {  # data types and constraints (e.g. "string": ["minLength", "patte
 
 
 class SchemaParser:
-    def create_test_dict(self):
+    def __init__(self):
         self.schema = SchemaParser.read_schema()
         self.type_definitions = self.schema["definitions"]
         self.tool_properties = self.type_definitions.pop("tool")["properties"]  # extract tool type into own variable
+        self.test_dict = {
+            TYPE_STRING_STR: [],
+            TYPE_OBJECT_STR: [],
+            TYPE_ARRAY_STR: []
+        }
+
+    def create_test_dict(self):
         self.build_dict()
-        return test_dict
+        return self.test_dict
 
     @staticmethod
     def read_schema():
@@ -39,14 +40,11 @@ class SchemaParser:
 
     def build_dict(self):
         """
-        Description: Iterates first-level dictionary items to build type-based test dictionary.
+        Description: Iterates dictionary items to build type-based test dictionary.
         """
         for prop in self.tool_properties.keys():
             prop_def = self.tool_properties[prop]  # extract definition of current property
             self.handle_prop(prop_def, [prop])  # parse the prop
-
-
-    # TODO parse special type definitions and save type info (e.g. urlftpType)
 
     @staticmethod
     def is_predefined_type(prop: object):
@@ -65,7 +63,7 @@ class SchemaParser:
     def handle_ref_type(self, reference: str, path_to_prop: list):
         reference_name = reference.split('/')[-1]
         reference_definition = self.type_definitions[reference_name]
-        print(f"reference {path_to_prop} '{reference_name}': {reference_definition}")
+        self.handle_prop(reference_definition, path_to_prop)
 
     def handle_prop(self, prop: object, path_to_prop: list):
         """
@@ -74,9 +72,11 @@ class SchemaParser:
         if SchemaParser.is_predefined_type(prop):  # known type
             prop_type = prop["type"]
             if prop_type == TYPE_STRING_STR:
-                SchemaParser.create_dict_entry(prop, path_to_prop)
+                SchemaParser.create_dict_entry(self, prop, path_to_prop)
             elif prop_type == TYPE_ARRAY_STR:
                 self.parse_array(prop, path_to_prop)
+            elif prop_type == TYPE_OBJECT_STR:
+                self.parse_object(prop, path_to_prop)
             else:
                 raise RuntimeWarning(f"[WARNING]: Type '{prop_type}' is currently not being handled.")
         elif SchemaParser.is_ref_type(prop):  # schema-defined type
@@ -84,30 +84,31 @@ class SchemaParser:
 
     def parse_array(self, array_prop: object, path: list):
         """
-        Description: Handles an array type, extracting its restrictions and initiating further parsing (if required).
+        Description: Handles an 'array' type, extracting its restrictions and initiating further parsing (if required).
         """
         array_items = array_prop["items"]
 
         if "properties" in array_items:
-            path.extend(ARRAY_PATH_EXTENSION)
-
             properties = array_items["properties"]
             for prop_name, prop_def in zip(properties.keys(), properties.values()):
                 self.handle_prop(prop_def, path + [prop_name])
-
-            path[:] = [p for p in path if p not in ARRAY_PATH_EXTENSION]  # reverse path extension
         else:
             self.handle_prop(array_items, path)
 
-        SchemaParser.create_dict_entry(array_prop, path)
+        SchemaParser.create_dict_entry(self, array_prop, path)
 
-    @staticmethod
-    def parse_object(object_prop, path):
-        for prop_key, prop_val in object_prop["properties"].items():
-            SchemaParser.handle_prop(prop_val, prop_key)
+    def parse_object(self, object_prop: object, path: list):
+        """
+        Description: Handles an 'object' type.
+        """
+        properties = object_prop["properties"]
 
-    @staticmethod
-    def create_dict_entry(prop, path):
+        for prop in properties:
+            path.append(prop)
+            self.handle_prop(object_prop["properties"][prop], path)
+            path.remove(prop)
+
+    def create_dict_entry(self, prop, path):
         restriction_dict = {}
         prop_type = prop["type"]
 
@@ -117,7 +118,7 @@ class SchemaParser:
                 restriction_dict["path"] = path
 
         if restriction_dict != {}:
-            test_dict[prop_type].append({f"{path[len(path) - 1]}": restriction_dict})
+            self.test_dict[prop_type].append({f"{path[len(path) - 1]}": restriction_dict})
 
 
 parser = SchemaParser()
--- backend/elixirapp/tests/automized_tests/test_generator.py
+++ backend/elixirapp/tests/automized_tests/test_generator.py
@@ -1,12 +1,11 @@
 from schema_parser import SchemaParser
+import json
 
 
 class TestGenerator:
     def __init__(self):
         parser = SchemaParser()
-
         self.test_dict = parser.create_test_dict()
-        # print(self.test_dict)
 
 
 testgen = TestGenerator()
