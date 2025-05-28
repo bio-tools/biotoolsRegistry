a29be9833826ffbd4a990227ce8675318f861758
alexandradenk <xendi3008@gmail.com>
2025-05-28 14:16:51 +0200

    adapted query param dict

--- backend/elixirapp/tests/param_config.py
+++ backend/elixirapp/tests/param_config.py
@@ -2,9 +2,8 @@ import sys
 
 query_param_dict = {
     "page": {"valid": [1], "invalid": ["invalid", sys.maxsize, ""]},
-    "format": {"valid": ["json", "xml", "yaml"], "invalid": ["invalid", ""]}, # TODO look fallbacks
+    "format": {"valid": ["json", "xml", "yaml"], "invalid": ["invalid", ""]},
     "sort": {"valid": ["lastUpdate", "additionDate", "name"], "invalid": ["invalid", ""]},
-    # TODO affiliation and score
     "biotoolsID": {"valid": [], "invalid": ["invalid", ""]},
-    "ord": {"valid": ["desc", "asc"], "invalid": ["invalid", ""]}  # TODO why does "" work, check invalid string
+    "ord": {"valid": ["desc", "asc"], "invalid": ["invalid", ""]}
 }
