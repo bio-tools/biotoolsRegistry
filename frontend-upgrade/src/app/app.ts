7895d81fdbcbccdfe4b2a47c388368af7963b863
Ana Mendes <anamendesml@outlook.com>
2025-09-08 16:52:45 +0200

    create page footer component

--- frontend-upgrade/src/app/app.ts
+++ frontend-upgrade/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Home } from './pages/home/home';
+import { PageFooter } from './components/page-footer/page-footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Home, PageFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ frontend-upgrade/src/app/components/page-footer/page-footer.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-page-footer',
+  imports: [
+
+  ],
+  templateUrl: './page-footer.html',
+  styleUrl: './page-footer.scss'
+})
+export class PageFooter {
+
+}
