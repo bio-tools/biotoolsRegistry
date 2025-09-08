71d42dc29a2240e1d17ed8415d214b185475b352
Ana Mendes <anamendesml@outlook.com>
2025-09-08 16:53:18 +0200

    create navbar component

---
+++ frontend-upgrade/src/app/components/navigation/navigation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [],
+  templateUrl: './navigation.html',
+  styleUrl: './navigation.scss'
+})
+export class Navigation {
+
+}
