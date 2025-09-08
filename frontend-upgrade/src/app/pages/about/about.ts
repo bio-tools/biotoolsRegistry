728b161f2b88e37265e1c2cb713340f4be9997a9
Ana Mendes <anamendesml@outlook.com>
2025-09-08 17:04:14 +0200

    create about page

---
+++ frontend-upgrade/src/app/pages/about/about.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  imports: [],
+  templateUrl: './about.html',
+  styleUrl: './about.scss'
+})
+export class About {
+
+}
