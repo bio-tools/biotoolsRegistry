2e665158180992e2963938b442512cbace69d2fb
anitamnd <anamendesml@outlook.com>
2025-09-18 18:30:34 +0200

    inject httpclient

--- frontend-upgrade/src/app/app.config.ts
+++ frontend-upgrade/src/app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes)
--- frontend-upgrade/src/app/app.routes.ts
+++ frontend-upgrade/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { About } from './pages/about/about';
 import { Home } from './pages/home/home';
+import { Search } from './pages/search/search';
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'home', redirectTo: '', pathMatch: 'full' },
-    { path: 'about', component: About }
+    { path: 'about', component: About },
+    { path: 't', component: Search }
 ];
