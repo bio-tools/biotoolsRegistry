e453563a04753c46c9d0113476f832a1f208f861
anitamnd <anamendesml@outlook.com>
2025-09-23 15:08:53 +0200

    fix colors and max-width

--- frontend-upgrade/src/app/app.ts
+++ frontend-upgrade/src/app/app.ts
@@ -1,15 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
 import { PageFooter } from './components/page-footer/page-footer';
 import { Navigation } from './components/navigation/navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Navigation, PageFooter],
+  imports: [RouterOutlet, Navigation, PageFooter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend-upgrade');
+  protected readonly title = signal('bio.tools');
 }
