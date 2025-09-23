9ccfa4abcfc22c0c3c601dbff3d11c463a1c9d83
anitamnd <anamendesml@outlook.com>
2025-09-23 14:23:41 +0200

    add router to navbar

--- frontend-upgrade/src/app/components/navigation/navigation.ts
+++ frontend-upgrade/src/app/components/navigation/navigation.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   imports: [
     MatButton,
     MatIcon,
-    MatIconButton
+    MatIconButton,
+    RouterLink
   ],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss'
