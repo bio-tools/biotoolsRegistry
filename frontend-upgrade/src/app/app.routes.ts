1a20b3e57118cc6f08827424b1baf7472b97238d
anitamnd <anamendesml@outlook.com>
2025-09-23 14:22:49 +0200

    add max-width to main content

--- frontend-upgrade/src/app/app.routes.ts
+++ frontend-upgrade/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { Home } from './pages/home/home';
 import { Search } from './pages/search/search';
 import { Domains } from './pages/domains/domains';
 import { Communities } from './pages/communities/communities';
+import { ToolPage } from './pages/tool-page/tool-page';
+import { ToolEdit } from './pages/tool-edit/tool-edit';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -11,5 +13,9 @@ export const routes: Routes = [
     { path: 'about', component: About },
     { path: 't', component: Search },
     { path: 'domains', component: Domains },
-    { path: 'communities', component: Communities }
+    { path: 'communities', component: Communities },
+    { path: 'tool/:id/edit', component: ToolEdit },
+    { path: 'tool/:id', component: ToolPage },
+    { path: ':id/edit', component: ToolEdit },
+    { path: ':id', component: ToolPage }
 ];
