89b1457c9937f0e737dfe1e434290083c5f94cf3
Ana Isabel da Silva Mendes <anamendesml@outlook.com>
2025-09-29 12:11:54 +0200

    config social account settings in frontend

---
+++ frontend/js/config.js
@@ -0,0 +1,8 @@
+// Configuration file for environment-specific settings
+angular.module('elixir_front').constant('AppConfig', {
+	ORCID_CLIENT_ID: '',
+	ORCID_REDIRECT_URI: '',
+	GITHUB_CLIENT_ID: '',
+	GITHUB_REDIRECT_URI: '',
+	GITHUB_SCOPE: ''
+});
