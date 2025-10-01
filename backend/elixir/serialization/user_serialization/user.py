bd98d1680c275555c095f1878314a7cba47ae02a
Ana Isabel da Silva Mendes <anamendesml@outlook.com>
2025-10-01 16:49:38 +0200

    refactor user serialization: consolidate user details into CustomUserDetailsSerializer and add email verification check

--- backend/elixir/serialization/user_serialization/user.py
+++ backend/elixir/serialization/user_serialization/user.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from elixir.models import *
-from dj_rest_auth.serializers import PasswordResetSerializer#, UserDetailsSerializer
+from dj_rest_auth.serializers import PasswordResetSerializer
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from allauth.account.adapter import get_adapter
+from allauth.account.models import EmailAddress
+from allauth.socialaccount.models import SocialAccount
+
+from elixir.models import *
+from elixir.serialization.resource_serialization.resource import ResourceNameSerializer
+
 import re
 
 # custom serializer just for enabling HTML emails
@@ -38,4 +43,111 @@ class UserNameSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
 		fields = ('username',)
+	
+
+# user details serializer
+class CustomUserDetailsSerializer(serializers.ModelSerializer):
+	resources = serializers.SerializerMethodField('get_resource')
+	sharedResources = serializers.SerializerMethodField('get_shared_resource')
+	# subdomains = serializers.SerializerMethodField()
+	requests_count = serializers.SerializerMethodField('get_resource_request_count')
+	socialAccounts = serializers.SerializerMethodField('get_social_accounts')
+	email_verified = serializers.SerializerMethodField('get_email_verified')
+
+	class Meta:
+		model = User
+		fields = ('username', 'email', 'resources', 'sharedResources', 'is_superuser', 'requests_count', 'socialAccounts', 'email_verified')
+
+	def get_resource(self, user):
+		resources = Resource.objects.filter(visibility=1, owner=user)
+		serializer = ResourceNameSerializer(instance=resources, many=True)
+		return serializer.data
+
+	def get_shared_resource(self, user):
+		resources = Resource.objects.filter(visibility=1, editPermission__authors__user=user)
+		serializer = ResourceNameSerializer(instance=resources, many=True)
+		return serializer.data
+
+	def get_resource_request_count(self, user):
+		if user.is_superuser:
+			return ResourceRequest.objects.filter(completed=False).count()
+		return ResourceRequest.objects.filter(resource__owner=user, completed=False).count()
+
+	def get_social_accounts(self, user):
+		social_accounts = SocialAccount.objects.filter(user=user)
+		return [
+			{
+				'id': account.id,
+				'provider': account.provider,
+				'uid': account.uid,
+				'extra_data': {
+					'login': account.extra_data.get('login', ''),
+					'name': account.extra_data.get('name', ''),
+					'email': account.extra_data.get('email', ''),
+					'orcid': account.extra_data.get('orcid', ''),
+					'given_names': account.extra_data.get('given-names', ''),
+					'family_name': account.extra_data.get('family-name', ''),
+				}
+			}
+			for account in social_accounts
+		]
+
+	def get_email_verified(self, user):
+		"""
+		Check if the user's primary email address is verified
+		"""
+		try:
+			email_address = EmailAddress.objects.get(user=user, primary=True)
+			return email_address.verified
+		except EmailAddress.DoesNotExist:
+			# If no EmailAddress exists, consider it unverified
+			return False
+
+	def update(self, instance, validated_data):
+		"""
+		Handle email address synchronization
+		"""
+		old_email = instance.email
+		new_email = validated_data.get('email', old_email)
+
+		if old_email == new_email:
+			raise serializers.ValidationError({'email': 'Email address is the same as the current one.'})
+
+		# Check for duplicates before any updates
+		existing_email = EmailAddress.objects.filter(email=new_email).exclude(user=instance).first()
+		if existing_email:
+			raise serializers.ValidationError({'email': f"Email {new_email} is already in use"})
+			
+
+		instance = super().update(instance, validated_data)
+		self._update_email_address(instance, old_email, new_email)
 		
+		return instance
+	
+	def _update_email_address(self, user, old_email, new_email):
+		"""
+		Update or create EmailAddress entries when user email changes
+		"""
+		try:
+			# Try to find existing primary email address
+			try:
+				old_email_obj = EmailAddress.objects.get(user=user, primary=True)
+				# Update existing primary email address
+				old_email_obj.email = new_email
+				old_email_obj.verified = False  # New email needs verification
+				old_email_obj.save()
+			except EmailAddress.DoesNotExist:
+				# Create new primary email address if none exists
+				EmailAddress.objects.create(
+					user=user,
+					email=new_email,
+					primary=True,
+					verified=False
+				)
+			
+			# Remove any non-primary email addresses with the old email
+			EmailAddress.objects.filter(user=user, email=old_email, primary=False).delete()
+			
+		except Exception as e:
+			# re-raise to prevent user update if email sync fails
+			raise
--- backend/elixir/serializers.py
+++ backend/elixir/serializers.py
@@ -1,19 +1,9 @@
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
+from rest_framework.response import Response
+
 from elixir.models import *
 from elixir.validators import *
 from elixir.exceptions import *
-import re, json
-from django.contrib.auth.models import User
-from rest_framework.response import Response
-from rest_framework.utils.urls import replace_query_param
-from allauth.account.adapter import get_adapter
-from allauth.account.models import EmailAddress
-from allauth.socialaccount.models import SocialAccount
-from django.db.models.signals import pre_save, post_save
-from django.conf import settings
-from elixir.request_handling import ResourceRequestHandler
-from random import randint
 
 from elixir.serialization.resource_serialization.domain import *
 from elixir.serialization.resource_serialization.edam import *
@@ -35,77 +25,3 @@ from elixir.serialization.resource_serialization.version import *
 from elixir.serialization.workflow_serialization.workflow import *
 from elixir.serialization.user_serialization.user import *
 from elixir.serialization.issues_serialization.issues import *
-
-
-# # just get the versions and latest
-# class VersionLatestSerializer(ResourceSerializer):
-
-# 	class Meta:
-# 		model = Resource
-# 		fields = ('versionId','version','latest',)
-
-
-class UserSerializer(serializers.ModelSerializer):
-	resources = serializers.SerializerMethodField('get_resource')
-	sharedResources = serializers.SerializerMethodField('get_shared_resource')
-	# subdomains = serializers.SerializerMethodField()
-	requests_count = serializers.SerializerMethodField('get_resource_request_count')
-	socialAccounts = serializers.SerializerMethodField('get_social_accounts')
-	email_verified = serializers.SerializerMethodField('get_email_verified')
-
-	class Meta:
-		model = User
-		fields = ('username', 'email', 'resources', 'sharedResources', 'is_superuser', 'requests_count', 'socialAccounts', 'email_verified')
-
-	def get_resource(self, user):
-		resources = Resource.objects.filter(visibility=1, owner=user)
-		serializer = ResourceNameSerializer(instance=resources, many=True)
-		return serializer.data
-
-	def get_shared_resource(self, user):
-		resources = Resource.objects.filter(visibility=1, editPermission__authors__user=user)
-		serializer = ResourceNameSerializer(instance=resources, many=True)
-		return serializer.data
-
-	def get_resource_request_count(self, user):
-		if user.is_superuser:
-			return ResourceRequest.objects.filter(completed=False).count()
-		return ResourceRequest.objects.filter(resource__owner=user, completed=False).count()
-
-	def get_social_accounts(self, user):
-		social_accounts = SocialAccount.objects.filter(user=user)
-		return [
-			{
-				'id': account.id,
-				'provider': account.provider,
-				'uid': account.uid,
-				'extra_data': {
-					'login': account.extra_data.get('login', ''),
-					'name': account.extra_data.get('name', ''),
-					'email': account.extra_data.get('email', ''),
-					'orcid': account.extra_data.get('orcid', ''),
-					'given_names': account.extra_data.get('given-names', ''),
-					'family_name': account.extra_data.get('family-name', ''),
-				}
-			}
-			for account in social_accounts
-		]
-
-	def get_email_verified(self, user):
-		"""
-		Check if the user's primary email address is verified
-		"""
-		try:
-			email_address = EmailAddress.objects.get(user=user, primary=True)
-			return email_address.verified
-		except EmailAddress.DoesNotExist:
-			# If no EmailAddress exists, consider it unverified
-			return False
-
-	# def get_subdomains(self, user):
-	# 	subdomains = Domain.objects.filter(owner=user)
-	# 	serializer = SubdomainNameSerializer(instance=subdomains, many=True)
-	# 	return serializer.data
-		
-
-
