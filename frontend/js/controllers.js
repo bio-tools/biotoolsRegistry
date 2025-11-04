bc70625b8efdce55faf2d2912a2728c20900d739
Ana Isabel da Silva Mendes <anamendesml@outlook.com>
2025-11-04 12:30:28 +0100

    support external JSON data loading via URL or query parameter

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1373,12 +1373,55 @@ angular.module('elixir_front.controllers', [])
 	// })
 
 }])
-.controller('ToolCreateController', ['$scope', '$controller', '$timeout', 'ToolListConnection', 'ToolCreateValidator', 'User', '$stateParams',  'CommunityCollection',function($scope, $controller, $timeout, ToolListConnection, ToolCreateValidator, User, $stateParams, CommunityCollection){
+.controller('ToolCreateController', ['$scope', '$controller', '$timeout', 'ToolListConnection', 'ToolCreateValidator', 'User', '$stateParams',  'CommunityCollection', '$location', '$http',function($scope, $controller, $timeout, ToolListConnection, ToolCreateValidator, User, $stateParams, CommunityCollection, $location, $http){
 	// inherit common controller
 	$controller('ToolEditController', {$scope: $scope});
 	$scope.orderby = 'text';
 	// sets which controller is in use, so the HTML can adapt
 	$scope.controller = 'create';
+		
+	$timeout(function() {
+        // Populate from external URL or query param ---
+        //  - ?json_url=https://example.com/tool.json
+        //  - ?json=<urlencoded JSON or base64-encoded JSON>
+        var search = $location.search();
+        if (search.json_url) {
+            $http.get(search.json_url).then(function(resp){
+                if (resp.data) {
+					// ensure digest cycle completes
+                    $timeout(function() {
+                        $scope.software = resp.data;
+                    }, 0);
+                }
+            }, function(err){
+                // ignore
+            });
+        } else if (search.json) {
+            var raw = search.json;
+            try {
+                // try decodeURIComponent first
+                var decoded = decodeURIComponent(raw);
+                var parsed = JSON.parse(decoded);
+
+				$timeout(function() {
+                    $scope.software = parsed;
+                }, 0);
+            } catch (e1) {
+                try {
+                    // fallback: base64
+                    var decodedB64 = atob(raw);
+                    var parsed = JSON.parse(decodedB64);
+
+					$timeout(function() {
+                        $scope.software = parsed;
+                    }, 0);
+                } catch (e2) {
+					// ignore
+                }
+            }
+        }
+    },100);
+
 
 	// initially set the ID to change automatically when name is modified
 	$scope.biotoolsIDDisabled = true;
@@ -1404,8 +1447,6 @@ angular.module('elixir_front.controllers', [])
 			$scope.editIdButtonText = 'From Name';
 		}
 	}
-
-	
 
 	$scope.validateButtonClick = function() {
 		$timeout(function() {
