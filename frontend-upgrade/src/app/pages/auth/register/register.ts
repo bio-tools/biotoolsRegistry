bb6f5e4f07df9b00a4d69a80374254c3acb55af1
Ana Isabel da Silva Mendes <anamendesml@outlook.com>
2025-11-19 15:04:03 +0100

    create register component

---
+++ frontend-upgrade/src/app/pages/auth/register/register.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.html',
+  styleUrl: './register.scss'
+})
+export class Register {
+
+}
