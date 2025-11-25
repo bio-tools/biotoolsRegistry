86940a752c0d6b537e70a2d9acf5e9522755b9a2
Ana Isabel da Silva Mendes <anamendesml@outlook.com>
2025-11-25 13:41:22 +0100

    conditional search bar in navbar

--- frontend-upgrade/src/app/components/navigation/navigation.ts
+++ frontend-upgrade/src/app/components/navigation/navigation.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { SearchBar } from '../search-bar/search-bar';
 
 @Component({
   selector: 'app-navigation',
   imports: [
+    SearchBar,
     MatButton,
     MatIcon,
     MatIconButton,
@@ -16,4 +18,17 @@ import { RouterLink } from '@angular/router';
 })
 export class Navigation {
 
+  isHomePage = false;
+
+  constructor(private router: Router) {
+    const path = this.router.url.split('?')[0].split('#')[0];
+    this.isHomePage = path === '/';
+
+    this.router.events.subscribe(e => {
+      if (e instanceof NavigationEnd) {
+        const path = this.router.url.split('?')[0].split('#')[0];
+        this.isHomePage = path === '/';
+      }
+    });
+  }
 }
