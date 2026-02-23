d8124f9263a203dc45ffcbd44f31c55625f1d450
anitamnd <anamendesml@outlook.com>
2026-02-23 14:41:47 +0100

    fix api urls for swagger

--- backend/elixirapp/urls.py
+++ backend/elixirapp/urls.py
@@ -1,50 +1,66 @@
 """elixirapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-	https://docs.djangoproject.com/en/1.8/topics/http/urls/
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
 Examples:
 Function views
-	1. Add an import:  from my_app import views
-	2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
-	1. Add an import:  from other_app.views import Home
-	2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-	1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from dj_rest_auth.views import PasswordResetView, PasswordResetConfirmView
-from dj_rest_auth.registration.views import SocialAccountListView, SocialAccountDisconnectView
+from dj_rest_auth.registration.views import (
+    SocialAccountListView,
+    SocialAccountDisconnectView,
+)
 from django.contrib import admin
 from django.urls import include, path, re_path
 from elixir import views
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-	re_path(r'^admin/', admin.site.urls),
-	path('rest-auth/', include('dj_rest_auth.urls')),
-	path('rest-auth/registration/',
-		 include('dj_rest_auth.registration.urls')),
-
-	# Password reset
-	path('rest-auth/password/reset/',
-		 PasswordResetView.as_view(),
-		 name='rest_password_reset'
-		 ),
-	path('rest-auth/password/reset/confirm/<uidb64>/<token>/',
-		 PasswordResetConfirmView.as_view(),
-		 name='password_reset_confirm'),
-	path('accounts/', include('allauth.urls')),
-	
-	# API Documentation
-	path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-	path('api/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-	path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-	
-	path('', include('elixir.urls')),
-    
-	# Social auth
-    path('rest-auth/orcid/', views.OrcidLogin.as_view(), name='orcid_login'),
-    path('rest-auth/orcid/connect/', views.OrcidConnect.as_view(), name='orcid_connect'),
-    path('rest-auth/socialaccounts/', SocialAccountListView.as_view(), name='social_account_list'),
-    path('rest-auth/socialaccounts/<int:pk>/disconnect/', SocialAccountDisconnectView.as_view(), name='social_account_disconnect'),
+    re_path(r"^admin/", admin.site.urls),
+    path("rest-auth/", include("dj_rest_auth.urls")),
+    path("rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    # Password reset
+    path(
+        "rest-auth/password/reset/",
+        PasswordResetView.as_view(),
+        name="rest_password_reset",
+    ),
+    path(
+        "rest-auth/password/reset/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path("accounts/", include("allauth.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("", include("elixir.urls")),
+    # Swagger for /api
+    re_path(
+        r"^$", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
+    ),
+    # Social auth
+    path("rest-auth/orcid/", views.OrcidLogin.as_view(), name="orcid_login"),
+    path(
+        "rest-auth/orcid/connect/", views.OrcidConnect.as_view(), name="orcid_connect"
+    ),
+    path(
+        "rest-auth/socialaccounts/",
+        SocialAccountListView.as_view(),
+        name="social_account_list",
+    ),
+    path(
+        "rest-auth/socialaccounts/<int:pk>/disconnect/",
+        SocialAccountDisconnectView.as_view(),
+        name="social_account_disconnect",
+    ),
 ]
