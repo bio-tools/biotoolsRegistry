cdfa1af62799b8bd8bcc6eeb0f808cd786fcf30b
anitamnd <anamendesml@outlook.com>
2026-02-24 11:33:03 +0100

    add orcid to requests table

--- backend/elixir/serialization/resource_serialization/resourceRequest.py
+++ backend/elixir/serialization/resource_serialization/resourceRequest.py
@@ -5,47 +5,77 @@ from elixir.request_handling import ResourceRequestHandler
 
 
 class ResourceRequestSerializer(serializers.ModelSerializer):
-	username = serializers.CharField(allow_blank=False, max_length=25, min_length=1, source="user.username")
-	email = serializers.EmailField(source="user.email") 
-	resourceId = serializers.CharField(allow_blank=False, min_length=1, source="resource.biotoolsID")
+    username = serializers.CharField(
+        allow_blank=False, max_length=25, min_length=1, source="user.username"
+    )
+    email = serializers.EmailField(source="user.email")
+    orcid = serializers.CharField(
+        allow_blank=True,
+        max_length=25,
+        source="user.socialAccounts.filter(provider='orcid').get('uid', '')",
+    )
+    resourceId = serializers.CharField(
+        allow_blank=False, min_length=1, source="resource.biotoolsID"
+    )
 
-	class Meta:
-		model = ResourceRequest
-		fields = ('type', 'completed', 'accepted', 'username', 'email', 'requestId', 'resourceId',)
+    class Meta:
+        model = ResourceRequest
+        fields = (
+            "type",
+            "completed",
+            "accepted",
+            "username",
+            "email",
+            "orcid",
+            "requestId",
+            "resourceId",
+        )
 
-	def create(self, validated_data):
-		request = ResourceRequest.objects.create()
-		request.user = User.objects.get(username=validated_data.get('user')['username'])
-		request.resource = Resource.objects.filter(visibility=1).get(biotoolsID__iexact=validated_data.get('resource')['biotoolsID'])
-		request.type = validated_data.get('type')
-		request.requestId = validated_data.get('requestId')
-		request.save()
-		return request
+    def create(self, validated_data):
+        request = ResourceRequest.objects.create()
+        request.user = User.objects.get(username=validated_data.get("user")["username"])
+        request.resource = Resource.objects.filter(visibility=1).get(
+            biotoolsID__iexact=validated_data.get("resource")["biotoolsID"]
+        )
+        request.type = validated_data.get("type")
+        request.requestId = validated_data.get("requestId")
+        request.save()
+        return request
 
-	def validate_type(self, attrs):
-		enum = ENUMValidator(['editing', 'ownership'])
-		attrs = enum(attrs)
-		return attrs
+    def validate_type(self, attrs):
+        enum = ENUMValidator(["editing", "ownership"])
+        attrs = enum(attrs)
+        return attrs
 
-	def validate(self, attrs):
-		if Resource.objects.filter(visibility=1).filter(biotoolsID__iexact=attrs['resource']['biotoolsID']).count() == 0:
-			raise serializers.ValidationError("Requested resource does not exist for specified 'resourceId'")
-		return attrs
+    def validate(self, attrs):
+        if (
+            Resource.objects.filter(visibility=1)
+            .filter(biotoolsID__iexact=attrs["resource"]["biotoolsID"])
+            .count()
+            == 0
+        ):
+            raise serializers.ValidationError(
+                "Requested resource does not exist for specified 'resourceId'"
+            )
+        return attrs
 
 
 class ResourceRequestConcludeSerializer(serializers.ModelSerializer):
-	accept = serializers.BooleanField(source="accepted")
+    accept = serializers.BooleanField(source="accepted")
 
-	class Meta:
-		model = ResourceRequest
-		fields = ('accept', 'requestId',)
+    class Meta:
+        model = ResourceRequest
+        fields = (
+            "accept",
+            "requestId",
+        )
 
-	def update(self, instance, validated_data):
-		instance.completed = True
-		instance.accepted = validated_data.get('accepted', instance.accepted)
-		instance.completedBy = self.context['user']
-		if instance.accepted is True:
-			request_handler = ResourceRequestHandler()
-			request_handler.proccessRequest(instance)
-		instance.save()
-		return instance
+    def update(self, instance, validated_data):
+        instance.completed = True
+        instance.accepted = validated_data.get("accepted", instance.accepted)
+        instance.completedBy = self.context["user"]
+        if instance.accepted is True:
+            request_handler = ResourceRequestHandler()
+            request_handler.proccessRequest(instance)
+        instance.save()
+        return instance
